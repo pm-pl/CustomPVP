4b84ae23b3dd78d3d42f3a3c452e941f3bd16d6c
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-08-18 20:28:24 -0300

    Fixed

--- src/HenryDM/CustomPVP/Events/SoupPvP.php
+++ src/HenryDM/CustomPVP/Events/SoupPvP.php
@@ -2,6 +2,7 @@
 
 namespace HenryDM\CustomPVP\Events;
 
+
 use pocketmine\player\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerItemUseEvent;
@@ -9,15 +10,21 @@ use pocketmine\plugin\PluginBase;
 
 class SoupPvP extends PluginBase implements Listener {
 
-    public function onPlayerInteract(PlayerInteractEvent $event) : void {
-        if($this->main->getConfig()->get("soup-pvp") === true) {
+private $main;
+
+	public function __construct(Main $main) {
+		$this->main = $main;
+	}
+
+        public function onPlayerInteract(PlayerInteractEvent $event) : void {
+          if($this->main->getConfig()->get("soup-pvp") === true) {
             $player = $event->getPlayer();
             $item = $event->getItem();
             $health = $player->getHealth();
-            if($player->getInventory()->getItemInHand()->getId("459")) {
-              if($player->getHealth() == $player->getMaxHealth()) {
-                $event->cancel();	
-              } else { 
+              if($player->getInventory()->getItemInHand()->getId("459")) {
+                if($player->getHealth() == $player->getMaxHealth()) {
+                  $event->cancel();	
+                } else { 
                     $player->setHealth($health + $this->main->getConfig()->get("regenerate-level"));
 		    $player->sendActionBarMessage($this->main->getConfig()->get("soup-message"));
                     $player->getInventory()->removeItem(Item::get($item->getId(), 0, 1));
