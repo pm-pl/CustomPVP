a43988da99181b53e3e97f33149a6c7bc4a3d79b
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-08-18 17:38:43 -0300

    Add Feature

---
+++ src/HenryDM/CustomPVP/Events/Message.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace HenryDM\CustomPVP\Events;
+
+use pocketmine\player\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\Event;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\world\World;
+use HenryDM\CustomPVP\Main;
+
+class EventListener implements Listener {
+
+private $main;
+
+    public function onDeath(PlayerDeathEvent $event): void {
+      if($this->main->getConfig()->get("message") === true) {		
+       $player = $event->getPlayer();
+        $cause = $player->getLastDamageCause();
+        if($player instanceof Player) {
+            switch ($cause->getCause()) {
+                case EntityDamageEvent::CAUSE_ENTITY_ATTACK:
+                    if($cause instanceof EntityDamageByEntityEvent) {
+                        $damager = $cause->getDamager();
+                        if($damager instanceof Player) {
+                            $message = str_replace(["{victin}", "{killer}"], [$event->getPlayer()->getName(), $damager->getName()], $this->main->getConfig()->get("kill-message"));
+                            $event->setDeathMessage($message);
+                        }
+                    }
+			}
+		}
+	}
+}	
