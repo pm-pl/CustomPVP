9fb7cb3c18f1d7203340ec5df97d465c545ecf69
Petter <107489114+PetterLight@users.noreply.github.com>
2022-08-18 22:05:20 -0300

    Update

---
+++ src/HenryDM/CustomPVP/Events/KnockBack.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace HenryDM\CustomPVP\Events;
+
+use HenryDM\CustomPVP\Main;
+use pocketmine\event\Listener;
+use pocketmine\event\Event;
+use pocketmine\utils\Config;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+class KnockBack implements Listener {
+
+private $main;
+
+	public function __construct(Main $main) {
+		$this->main = $main;
+	}
+	          public function onEntity(EntityDamageByEntityEvent $event) : void {	
+             if($this->main->getConfig()->get("knockback") === true) {
+		            $event->setKnockBack($this->main->getConfig()->get("knockback-level") * $event->getKnockBack());
+	      }
+    }	 	
+}	
