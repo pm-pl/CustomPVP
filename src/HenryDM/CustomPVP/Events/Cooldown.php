640b2b2f87f9cc66f537c898c48a043d03caf978
Petter <107489114+PetterLight@users.noreply.github.com>
2022-08-18 22:00:48 -0300

    Add Cooldown to paste

---
+++ src/HenryDM/CustomPVP/Events/Cooldown.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace HenryDM\CustomPVP\Events;
+
+use HenryDM\CustomPVP\Main;
+use pocketmine\event\Listener;
+use pocketmine\event\Event;
+use pocketmine\utils\Config;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+class EventListener implements Listener {
+
+private $main;
+
+	public function __construct(Main $main) {
+		$this->main = $main;
+	}
+         	public function onDamage(EntityDamageEvent $event) : void {
+             if($this->main->getConfig()->get("cooldown") === true) {
+		           $event->setAttackCooldown($event->getAttackCooldown() - $this->main->getConfig()->get("cooldown-time"));
+	      }
+    }	 	
+}	
