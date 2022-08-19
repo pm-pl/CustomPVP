9d28f8c84c2fc55dad206f28740589d49b54c0e4
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-08-19 07:31:52 -0300

    particle Function
    Create initial commit to add Particle when players kill other.

---
+++ src/HenryDM/CustomPVP/Events/Particles.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace HenryDM\customPVP\Events;
+
+use HenryDM\CustomPVP\Main;
+use pocketmine\player\Player;
+use pocketmine\world\World;
+use pocketmine\event\PlayerDeathEvent;
+use pocketmine\world\particle\Particle;
+
+class Particle implements Listener {
+
+private $main;
+ 
+         public function __construct(Main $main) {
+            $this->main = $main;
+	}
+
+               public function onPlayerDeath(PlayerDeathEvent $event) { 
+                 $world = $player()->getWorld
+                 $player = $event()->getPlayer();
+                 $position = $player()->getPosition();
+                 $config = $this->main->getConfig()->get();
+
+
+  }
+}
