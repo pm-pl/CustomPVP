f9cb068d24b0f8f23536cd4c6cadeb17dca0433e
Petter <107489114+PetterLight@users.noreply.github.com>
2022-08-18 22:20:03 -0300

    Add file

---
+++ src/HenryDM/CustomPVP/Events/HealthRestore.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace HenryDM\CustomPVP\Events;
+
+use HenryDM\CustomPVP\Main;
+use pocketmine\event\Listener;
+use pocketmine\event\Event;
+use pocketmine\utils\Config;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+class Cooldown implements Listener {
+
+private $main;
+
+	public function __construct(Main $main) {
+		$this->main = $main;
+	}
+    public function onPlayerDeath(PlayerDeathEvent $event) : void{ 
+      if($this->main->getConfig()->get("restore-health") === true) {
+        $cause = $event->getPlayer()->getLastDamageCause();
+        if($cause instanceof EntityDamageByEntityEvent){
+            $damager = $cause->getDamager();
+            if($damager instanceof Player){
+                if(in_array($event->getPlayer()->getWorld()->getFolderName(), $this->main->getConfig()->get("restore-worlds"))){
+                    $damager->setHealth($damager->getMaxHealth());
+                }
+	           }
+          }			
+       }
+   }	
+}
+	/**
+	 * @return Main
+	 */
+	public function getMain(): Main {
+          return $this->main;
+}	
