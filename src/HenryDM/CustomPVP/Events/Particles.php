fc4b99a3b70263898bbc36ffba9deeb79c56724f
Petter <107489114+PetterLight@users.noreply.github.com>
2022-08-19 17:55:03 -0300

    Test

--- src/HenryDM/CustomPVP/Events/Particles.php
+++ src/HenryDM/CustomPVP/Events/Particles.php
@@ -42,12 +42,12 @@ class Particles implements Listener {
                           $world->addParticle($position->add(0, 1, 0), new CriticalParticle(1));
                           $world->addParticle($position->add(0, 0, 1), new CriticalParticle(1));
 	        }
-                        if($this->main->getConfig()->get("explode-particle") === true) {
-                          $world->addParticle($position, new ExplodeParticle(1));
-                          $world->addParticle($position, new ExplodeParticle(1));
-                          $world->addParticle($position->add(1, 0, 0), new ExplodeParticle(1));
-                          $world->addParticle($position->add(0, 1, 0), new ExplodeParticle(1));
-                          $world->addParticle($position->add(0, 0, 1), new ExplodeParticle(1));
+                        if($this->main->getConfig()->get("heart-particle") === true) {
+                          $world->addParticle($position, new HeartParticle(1));
+                          $world->addParticle($position, new HeartParticle(1));
+                          $world->addParticle($position->add(1, 0, 0), new HeartParticle(1));
+                          $world->addParticle($position->add(0, 1, 0), new HeartParticle(1));
+                          $world->addParticle($position->add(0, 0, 1), new HeartParticle(1));
 		}
             }
         }
