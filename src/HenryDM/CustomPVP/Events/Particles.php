954a0158c005b55f66412ad028905aa041f5801d
Petter <107489114+PetterLight@users.noreply.github.com>
2022-08-19 12:07:46 -0300

    Rewrite

--- src/HenryDM/CustomPVP/Events/Particles.php
+++ src/HenryDM/CustomPVP/Events/Particles.php
@@ -2,18 +2,18 @@
 
 namespace HenryDM\CustomPVP\Events;
 
-use pocketmine\event\Listener;
 use HenryDM\CustomPVP\Main;
 use pocketmine\player\Player;
 use pocketmine\world\Position;
 use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\Listener;
 use pocketmine\world\particle\CriticalParticle;
 use pocketmine\world\particle\ExplodeParticle;
 use pocketmine\world\particle\FlameParticle;
 use pocketmine\world\particle\HappyVillagerParticle;
 use pocketmine\world\particle\HeartParticle;
 use pocketmine\world\particle\LavaDripParticle;
-use pocketmine\world\particle\LavaParticle;
+use pocketmine\world\particle;
 use pocketmine\world\particle\PortalParticle;
 use pocketmine\world\particle\RedstoneParticle;
 use pocketmine\world\particle\SnowballPoofParticle;
@@ -30,9 +30,9 @@ class Particles implements Listener {
                  $player = $event->getEntity();
                  $position = $entity()->getPosition();
           
-                   if($this->main->getConfig()->get("particle") == true) {
+                   if($this->main->getConfig()->get("particle") === true) {
                     if(in_array($player()->getWorld()->getFolderName(), $this->main->getConfig()->get("particle-worlds"))) {
-                      if($this->main->getConfig()->get("critical-particle") == true) {
+                      if($this->main->getConfig()->get("critical-particle") === true) {
                          $world->addParticle($position, new CriticalParticle(1));
                          $world->addParticle($position, new CriticalParticle(1));
                          $world->addParticle($position->add(1, 0, 0), new CriticalParticle(1));
