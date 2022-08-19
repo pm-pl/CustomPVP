68c08e350f77e8ed66ad0d933d82270563deb7a9
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-19 22:25:04 +0700

    Code Improve

--- src/HenryDM/CustomPVP/Events/Cooldown.php
+++ src/HenryDM/CustomPVP/Events/Cooldown.php
@@ -2,29 +2,26 @@
 
 namespace HenryDM\CustomPVP\Events;
 
-use HenryDM\CustomPVP\Main;
 use pocketmine\event\Listener;
-use pocketmine\event\Event;
-use pocketmine\utils\Config;
+
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 
+use HenryDM\CustomPVP\Main;
+
 class Cooldown implements Listener {
 
-private $main;
+    public function __construct(private Main $main) {
+        $this->main = $main;
+    }
 
-	public function __construct(Main $main) {
-		$this->main = $main;
-	}
-         	public function onDamage(EntityDamageEvent $event) : void {
-             if($this->main->getConfig()->get("cooldown") === true) {
-		           $event->setAttackCooldown($event->getAttackCooldown() - $this->main->getConfig()->get("cooldown-time"));
+    public function onDamage(EntityDamageEvent $event) : void {
+        if($this->getMain()->cfg->get("cooldown") === true) {
+            $event->setAttackCooldown($event->getAttackCooldown() - $this->getMain()->cfg->get("cooldown-time"));
 	}
     }
-	/**
-	 * @return Main
-	 */
-	public function getMain(): Main {
-          return $this->main;
-	}	 	
+
+    public function getMain() : Main {
+        return $this->main;
+    }	 	
 }	
