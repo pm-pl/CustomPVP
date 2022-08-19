a57115423ad7dfc27a91fb3924e321816b18de1a
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-08-19 20:01:42 -0300

    Fix Crash

--- src/HenryDM/CustomPVP/Events/LeechingMode.php
+++ src/HenryDM/CustomPVP/Events/LeechingMode.php
@@ -3,7 +3,6 @@
 namespace HenryDM\CustomPVP\Events;
 
 use HenryDM\CustomPVP\Main;
-use pocketmine\player\Player;
 use pocketmine\entity\Entity;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
@@ -19,12 +18,8 @@ class LeechingMode implements Listener {
         public function onDamage(EntityDamageByEntityEvent $event) : void {
           if($this->main->getConfig()->get("leeching-mode") === true) {
             $player = $event->getEntity();
-                if($player->getHealth() == $player->getMaxHealth()) {
-                  $event->cancel();	
-                } else { 
                    if(in_array($player()->getWorld()->getFolderName(), $this->main->getConfig()->get("leeching-worlds"))) {
                      $player->setHealth($player->getHealth() + $this->main->getConfig()->get("leeching-level"));
-	       }
 	    }
         }
     }
