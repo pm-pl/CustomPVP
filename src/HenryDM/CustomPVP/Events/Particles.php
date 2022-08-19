4a5d8a3684fdd09785e863c6e61c61f1cfb678e7
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-08-19 10:15:34 -0300

    Use Entity Method

--- src/HenryDM/CustomPVP/Events/Particles.php
+++ src/HenryDM/CustomPVP/Events/Particles.php
@@ -6,43 +6,19 @@ use pocketmine\event\Listener;
 use HenryDM\CustomPVP\Main;
 use pocketmine\player\Player;
 use pocketmine\world\World;
-use pocketmine\event\PlayerDeathEvent;
+use pocketmine\event\EntityDeathEvent;
 use pocketmine\world\particle\Particle;
-use pocketmine\world\particle\BlockBreakParticle;
-use pocketmine\world\particle\BlockForceFieldParticle;
-use pocketmine\world\particle\BlockPunchParticle;
-use pocketmine\world\particle\BubbleParticle;
 use pocketmine\world\particle\CriticalParticle;
-use pocketmine\world\particle\DragonEggTeleportParticle;
-use pocketmine\world\particle\DustParticle;
-use pocketmine\world\particle\EnchantParticle;
-use pocketmine\world\particle\EnchantamentTableParticle;
-use pocketmine\world\particle\EndermanTeleportParticle;
-use pocketmine\world\particle\EntityFlameParticle;
 use pocketmine\world\particle\ExplodeParticle;
 use pocketmine\world\particle\FlameParticle;
-use pocketmine\world\particle\FloatingTextParticle;
 use pocketmine\world\particle\HappyVillagerParticle;
 use pocketmine\world\particle\HeartParticle;
-use pocketmine\world\particle\HugeExplodeParticle;
-use pocketmine\world\particle\HugeExplodeSeedParticle;
-use pocketmine\world\particle\InkParticle;
-use pocketmine\world\particle\InstantEnchantParticle;
-use pocketmine\world\particle\ItemBreakParticle;
 use pocketmine\world\particle\LavaDripParticle;
 use pocketmine\world\particle\LavaParticle;
-use pocketmine\world\particle\MobSpawnParticle;
 use pocketmine\world\particle\PortalParticle;
-use pocketmine\world\particle\PotionSplashParticle;
-use pocketmine\world\particle\RainSplashParticle;
 use pocketmine\world\particle\RedstoneParticle;
-use pocketmine\world\particle\SmokeParticle;
 use pocketmine\world\particle\SnowballPoofParticle;
-use pocketmine\world\particle\SplashParticle;
-use pocketmine\world\particle\SporeParticle;
-use pocketmine\world\particle\TerrainParticle;
-use pocketmine\world\particle\WaterDripParticle;
-use pocketmine\world\particle\WaterParticle;
+
 
 class Particles implements Listener {
 
@@ -52,14 +28,15 @@ private $main;
             $this->main = $main;
 	}
 
-               public function onPlayerDeath(PlayerDeathEvent $event) { 
-                 $world = $player()->getWorld();
-                 $player = $event()->getPlayer();
-                 $position = $player()->getPosition();
+               public function onEntityDeath(EntityDeathEvent $event) { 
+                 $world = $entity()->getWorld();
+                 $entity = $event()->getEntity();
+                 $position = $entity()->getPosition();
                  $config = $this->main->getConfig();
           
                    if($config("particle") == true) {
-                    if(in_array($event->getPlayer()->getWorld()->getFolderName(), $this->main->getConfig()->get("particle-worlds"))){
+                    if(in_array($event->getEntity()->getWorld()->getFolderName(), $this->main->getConfig()->get("particle-worlds"))){
+                      if($config("critical-particle") == true) {
                      $world->addParticle($position, new AngryVillagerParticle(1));
                      $world->addParticle($position, new AngryVillagerParticle(1));
                      $world->addParticle($position->add(1, 0, 0), new AngryVillagerParticle(1));
