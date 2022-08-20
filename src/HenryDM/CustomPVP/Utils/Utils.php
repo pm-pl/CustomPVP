028e706a1fe06085aefb29a363cde36b058a475c
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-20 18:07:31 +0700

    Update Utils.php

--- src/HenryDM/CustomPVP/Utils/Utils.php
+++ src/HenryDM/CustomPVP/Utils/Utils.php
@@ -4,8 +4,24 @@ namespace HenryDM\CustomPVP\Utils;
 
 use pocketmine\player\Player;
 
+use pocketmine\network\mcpe\protocol\PlaySoundPacket;
+
+use HenryDM\CustomPVP\Main;
+
 class Utils {
 
-// SOON
-
+    public function __construct(private Main $main) {
+        $this->main = $main;
+    }
+  
+    public function playSound(Player $player, string $soundName = " ", float $volume = 0, float $pitch = 0) {
+        $pk = new PlaySoundPacket();
+        $pk->x = $player->getPosition()->getX();
+        $pk->x = $player->getPosition()->getY();
+        $pk->x = $player->getPosition()->getZ();
+        $pk->soundName = $soundName;
+        $pk->volume = $volume;
+        $pk->pitch = $pitch;
+        $player->getNetworkSession()->senddataPacket($pk);
+    }
 }
