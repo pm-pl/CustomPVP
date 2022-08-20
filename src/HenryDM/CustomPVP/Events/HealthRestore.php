80a51b469bfce96d70070a0d62df825844f43728
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-20 13:53:04 +0700

    Update HealthRestore.php

--- src/HenryDM/CustomPVP/Events/HealthRestore.php
+++ src/HenryDM/CustomPVP/Events/HealthRestore.php
@@ -3,32 +3,36 @@
 namespace HenryDM\CustomPVP\Events;
 
 use pocketmine\player\Player;
-use pocketmine\event\player\PlayerDeathEvent;
-use HenryDM\CustomPVP\Main;
+
 use pocketmine\event\Listener;
-use pocketmine\event\Event;
-use pocketmine\utils\Config;
-use pocketmine\event\entity\EntityDamageEvent;
+
+use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+use HenryDM\CustomPVP\Main;
 
 class HealthRestore implements Listener {
 
-private $main;
+    public function __construct(private Main $main) {
+        $this->main = $main;
+    }
 
-	public function __construct(Main $main) {
-		$this->main = $main;
-	}
-             public function onPlayerDeath(PlayerDeathEvent $event) : void { 
-              if($this->main->getConfig()->get("restore-health") === true) {
-                $cause = $event->getPlayer()->getLastDamageCause();
-                 if($cause instanceof EntityDamageByEntityEvent){
-                   $damager = $cause->getDamager();
-                    if($damager instanceof Player){
-                       if(in_array($event->getPlayer()->getWorld()->getFolderName(), $this->main->getConfig()->get("restore-worlds"))){
-                          $damager->setHealth($damager->getMaxHealth());
-                  }
-              }
-          }			
-       }
-    }	
+    public function onPlayerDeath(PlayerDeathEvent $event) : void { 
+        if($this->main->getConfig()->get("restore-health") === true) {
+        $player = $event->getPlayer();
+        $cause = $player->getLastDamageCause();
+            if($cause instanceof EntityDamageByEntityEvent) {
+                $damager = $cause->getDamager();
+                if($damager instanceof Player) {
+                    if(in_array($player->getWorld()->getFolderName(), $this->getMain()->cfg->get("restore-worlds"))) {
+                        $damager->setHealth($damager->getMaxHealth());
+                    }
+                }
+            }			
+        }
+    }
+
+    public function getMain() : Main {
+        return $this->main;
+    }
 }
