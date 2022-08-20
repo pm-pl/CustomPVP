c6595069f877c68c3537783a31df40f39f63ebac
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-20 14:15:00 +0700

    Update KnockBack.php

--- src/HenryDM/CustomPVP/Events/KnockBack.php
+++ src/HenryDM/CustomPVP/Events/KnockBack.php
@@ -2,29 +2,23 @@
 
 namespace HenryDM\CustomPVP\Events;
 
-use HenryDM\CustomPVP\Main;
 use pocketmine\event\Listener;
-use pocketmine\event\Event;
-use pocketmine\utils\Config;
-use pocketmine\event\entity\EntityDamageEvent;
+
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 
 class KnockBack implements Listener {
 
-private $main;
-
-	public function __construct(Main $main) {
-		$this->main = $main;
-	}
-	          public function onEntity(EntityDamageByEntityEvent $event) : void {	
-             if($this->main->getConfig()->get("knockback") === true) {
-		            $event->setKnockBack($this->main->getConfig()->get("knockback-level") * $event->getKnockBack());
-	      }
+    public function __construct(private Main $main) {
+        $this->main = $main;
     }
-	/**
-	 * @return Main
-	 */
-	public function getMain(): Main {
-          return $this->main;
-	}	 	
-}	
+
+    public function onEntity(EntityDamageByEntityEvent $event) : void {	
+        if($this->getMain()->cfg->get("knockback") === true) {
+            $event->setKnockBack($this->getMain()->cfg->get("knockback-level") * $event->getKnockBack());
+        }
+    }
+
+    public function getMain() : Main {
+        return $this->main;
+    }	 	
+}
