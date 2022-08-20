9ea27d5afa78016ece8b14a3788e6e239ebed910
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-20 18:10:08 +0700

    Fix

--- src/HenryDM/CustomPVP/Events/SoupPvP.php
+++ src/HenryDM/CustomPVP/Events/SoupPvP.php
@@ -28,10 +28,10 @@ class SoupPvP implements Listener {
                 if ($health == $maxhealth) {
                     $event->cancel();	
                 } else {
-                     if (in_array($world->getFolderName(), $this->getMain()->cfg->get("soup-worlds"))) {
-                      $player->setHealth($health + $this->getMain()->cfg->get("regenerate-level"));
-		      $player->sendActionBarMessage($this->getMain()->cfg->get("soup-message"));
-                      $player->getInventory()->removeItem(ItemFactory::getInstance()->get($item->getId(), 0, 1));
+                    if (in_array($world->getFolderName(), $this->getMain()->cfg->get("soup-worlds"))) {
+                        $player->setHealth($health + $this->getMain()->cfg->get("regenerate-level"));
+		        $player->sendActionBarMessage($this->getMain()->cfg->get("soup-message"));
+                        $player->getInventory()->removeItem(ItemFactory::getInstance()->get($item->getId(), 0, 1));
 	            } 
                 }
 	    }
