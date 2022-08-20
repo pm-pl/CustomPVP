abe1d12fa0113982416307cd8ee8a818f253e583
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-20 18:34:20 +0700

    Add files via upload

---
+++ src/HenryDM/CustomPVP/Events/EnderPearlCooldown.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace HenryDM\CustomPVP\Events;
+
+use pocketmine\player\Player;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+
+use pocketmine\item\EnderPearl;
+
+use HenryDM\CustomPVP\Main;
+
+use function time;
+
+class EnderPearlCooldown implements Listener {
+
+    private array $cooldowns = [];
+
+    public function __construct(private Main $main) {
+        $this->main = $main;
+    }
+
+    public function onQuit(PlayerQuitEvenet $event) {
+        $player = $event->getPlayer();
+        if (isset($this->cooldowns[$player->getName()])) {
+            unset ($this->cooldowns[$player->getName()]);
+        }
+    }
+
+    public function onInteract(PlayerInteractEvent $event) {
+        $player = $event->getPlayer();
+        $item = $event->getItem();
+
+        if ($item instanceof EnderPearl) {
+            if (isset($this->cooldowns[$player->getName()]) and time() - $this->cooldowns[$player->getName()] < ) {
+                $event->cancel();
+                $time = $this->getMain->cfg->get("cooldowns-time") - (time() - $this->cooldowns[$player->getName()]);
+                $player->sendMessage(str_replace("{time}", $time, $thhis->getMain->cfg->get("cooldown-message")));
+            } else {
+                $this->cooldowns[$player->getName()] = (time() - $this->getMain()->cfg->get("cooldowns-time"));
+            }
+        }
+    }
+
+    public function getMain() : Main {
+        return $this->main;
+    }
+}
