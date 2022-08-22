4c81c1aadd7b7e85b37897bcb3eaf2a20fe771f1
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2022-08-22 20:45:13 +0700

    Nothing

--- src/HenryDM/CustomPVP/Events/EnderPearlCooldown.php
+++ src/HenryDM/CustomPVP/Events/EnderPearlCooldown.php
@@ -19,33 +19,33 @@ class EnderPearlCooldown implements Listener {
 
     private array $cooldowns = [];
 
-    public function __construct(private Main $main) {
-        $this->main = $main;
-    }
+//     public function __construct(private Main $main) {
+//         $this->main = $main;
+//     }
 
-    public function onQuit(PlayerQuitEvent $event) {
-        $player = $event->getPlayer();
-        if (isset($this->cooldowns[$player->getName()])) {
-            unset ($this->cooldowns[$player->getName()]);
-        }
-    }
+//     public function onQuit(PlayerQuitEvent $event) {
+//         $player = $event->getPlayer();
+//         if (isset($this->cooldowns[$player->getName()])) {
+//             unset ($this->cooldowns[$player->getName()]);
+//         }
+//     }
 
-    public function onInteract(PlayerInteractEvent $event) {
-        $player = $event->getPlayer();
-        $item = $event->getItem();
+//     public function onInteract(PlayerInteractEvent $event) {
+//         $player = $event->getPlayer();
+//         $item = $event->getItem();
 
-        if ($item instanceof EnderPearl) {
-            if (isset($this->cooldowns[$player->getName()]) and time() - $this->cooldowns[$player->getName()] < $this->getMain()->cfg->get("cooldowns-time")) {
-                $event->cancel();
-                $time = $this->getMain()->cfg->get("cooldowns-time") - (time() - $this->cooldowns[$player->getName()]);
-                $player->sendMessage(str_replace("{time}", $time, $this->getMain()->cfg->get("cooldown-message")));
-            } else {
-                $this->cooldowns[$player->getName()] = (time() - $this->getMain()->cfg->get("cooldowns-time"));
-            }
-        }
-    }
+//         if ($item instanceof EnderPearl) {
+//             if (isset($this->cooldowns[$player->getName()]) and time() - $this->cooldowns[$player->getName()] < $this->getMain()->cfg->get("cooldowns-time")) {
+//                 $event->cancel();
+//                 $time = $this->getMain()->cfg->get("cooldowns-time") - (time() - $this->cooldowns[$player->getName()]);
+//                 $player->sendMessage(str_replace("{time}", $time, $this->getMain()->cfg->get("cooldown-message")));
+//             } else {
+//                 $this->cooldowns[$player->getName()] = (time() - $this->getMain()->cfg->get("cooldowns-time"));
+//             }
+//         }
+//     }
 
-    public function getMain() : Main {
-        return $this->main;
-    }
-}
+//     public function getMain() : Main {
+//         return $this->main;
+//     }
+// }
