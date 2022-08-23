92c4a9601f8d23d6befccaecd8124b89fc1d20bf
Petter <107489114+PetterLight@users.noreply.github.com>
2022-08-23 16:11:40 -0300

    Fixed

--- src/HenryDM/CustomPVP/Events/DeathEffects.php
+++ src/HenryDM/CustomPVP/Events/DeathEffects.php
@@ -5,12 +5,13 @@ namespace HenryDM\CustomPVP\Events;
 use HenryDM\CustomPVP\Main;
 use pocketmine\event\Listener;
 
+use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\player\Player;
 use pocketmine\world\World;
-use pocketmine\entity\Effect;
 
 class DeathEffects implements Listener { 
 
@@ -22,12 +23,13 @@ class DeathEffects implements Listener {
         $player = $event->getPlayer();
         $world = $player->getWorld();
         $worldName = $world->getFolderName();
+        $damager = $damageCause->getDamager();
+        $damageCause = $player->getLastDamageCause();
         if($player instanceof Player) {
             if($this->getMain()->cfg->get("death-effects") === true) { 
                 if ($damageCause instanceof EntityDamageByEntityEvent) {
-                    $damager = $damageCause->getDamager();
                     if ($damager instanceof Player) {
-                        if(in_array($worldname(), $this->getMain()->cfg->get("effect-worlds"))) {
+                        if(in_array($worldName(), $this->getMain()->cfg->get("effect-worlds"))) {
                             $player->addEffect(new EffectInstance(Effect::getEffect(Effect::$this->getMain()->cfg->get("effect-name")), $this->getMain()->cfg->get("efffect-duration")* 1, 1, true));             
                     
                         }                        
@@ -35,5 +37,9 @@ class DeathEffects implements Listener {
                 }
             }
         }
+    }
+    
+    public function getMain() : Main {
+        return $this->main;
     }
 }
