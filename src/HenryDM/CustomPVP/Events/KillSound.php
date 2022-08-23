080f2b0a4c4302f9877d2fd4d7e309387185e124
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-08-23 09:38:07 -0300

    Update KillSound.php

--- src/HenryDM/CustomPVP/Events/KillSound.php
+++ src/HenryDM/CustomPVP/Events/KillSound.php
@@ -24,14 +24,24 @@ class KillSound implements Listener {
         $world = $player->getWorld();
         $damageCause = $player->getLastDamageCause();
         if ($this->getMain()->cfg->get("kill-sound") === true) {
-                
-if($this->getMain()->cfg->get("anvil-sound") === true) {
-                    $world->addSound($sound, new AnvilUseSound(1));
-                    $world->addSound($sound, new AnvilUseSound(1));
-                    $world->addSound($sound->add(1, 0, 0), new AnvilUseSound(1));
-                    $world->addSound($sound->add(0, 1, 0), new AnvilUseSound(1));
-                    $world->addSound($sound->add(0, 0, 1), new AnvilUseSound(1));
+        if (in_array($world->getFolderName(), $this->getMain()->cfg->get("particle-worlds"))) {
+        if($this->getMain()->cfg->get("anvil-sound") === true) {
+                    $world->addSound($position, new AnvilUseSound(1));
+                    $world->addSound($position, new AnvilUseSound(1));
+                    $world->addSound($position->add(1, 0, 0), new AnvilUseSound(1));
+                    $world->addSound($position->add(0, 1, 0), new AnvilUseSound(1));
+                    $world->addSound($position->add(0, 0, 1), new AnvilUseSound(1));
 	        }
+
+        if($this->getMain()->cfg->get("ghast-sound") === true) {
+                    $world->addSound($position, new GhastSound(1));
+                    $world->addSound($position, new GhastSound(1));
+                    $world->addSound($position->add(1, 0, 0), new GhastSound(1));
+                    $world->addSound($position->add(0, 1, 0), new GhastSound(1));
+                    $world->addSound($position->add(0, 0, 1), new GhastSound(1));
+	        }
+
+	      }
           }
      }
 
