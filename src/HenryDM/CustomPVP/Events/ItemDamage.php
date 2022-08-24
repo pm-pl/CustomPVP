2c5a4d61c3c0185ed8d5071da62bc6829ae14b7d
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-08-24 07:37:22 -0300

    Add change ItemDamage event

---
+++ src/HenryDM/CustomPVP/Events/ItemDamage.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace HenryDM\CustomPVP\Events;
+
+use HenryDM\CustomPVP\Main;
+use pocketmine\event\Listener;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\world\World;
+use pocketmine\player\Player;
+
+class ItemDamage implements Listener {
+
+    public function __construct(private Main $main) {
+   
+    }
+   
+       public function onDamage(EntityDamageByEntityEvent $event) {
+          $entity = $event->getEntity();
+          $world = $entity->getWorld();
+          $worldName = $world->getFolderName();
+          if($event instanceof EntityDamageByEntityEvent) {
+             $damaged = $event->getDamager();
+             if($damaged instanceof Player) {
+                if($this->getMain()->cfg->get("Item-damage") === true) {
+                  if(in_array($worldName, $this->getMain->cfg->get("damage-worlds"))) {
+
+# ================
+#   Wooden Sword 
+# ================
+
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::WOODEN_SWORD) {
+                       $event->setDamage($this->getMain->cfg->get("wooden-sword"));
+
+            }
+
+# ================
+#   Stone Sword 
+# ================
+
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::STONE_SWORD) {
+                       $event->setDamage($this->getMain->cfg->get("stone-sword"));
+
+            }
+
+# ================
+#   Iron Sword 
+# ================
+
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::IRON_SWORD) {
+                       $event->setDamage($this->getMain->cfg->get("wooden-sword"));
+
+            }
+
+# ================
+#   Golden Sword 
+# ================
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::GOLDEN_SWORD) {
+                       $event->setDamage($this->getMain->cfg->get("wooden-sword"));
+
+            }
+
+# ================
+#  Diamond Sword 
+# ================
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::DIAMOND_SWORD) {
+                       $event->setDamage($this->getMain->cfg->get("wooden-sword"));
+
+            }
+
+# ================
+#    Wooden Axe
+# ================
+
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::WOODEN_AXE) {
+                       $event->setDamage($this->getMain->cfg->get("wooden-axe"));
+
+            }
+
+# ================
+#    Stone Axe
+# ================
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::STONE_AXE) {
+                       $event->setDamage($this->getMain->cfg->get("stone-axe"));
+
+            }
+
+# ================
+#     Iron Axe
+# ================
+
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::IRON_AXE) {
+                       $event->setDamage($this->getMain->cfg->get("iron-axe"));
+
+            }
+
+# ================
+#   Diamond Axe
+# ================
+
+                    if($damaged->getInventory()->getItemInHand()->getId() == Item::DIAMOND_AXE) {
+                       $event->setDamage($this->getMain->cfg->get("diamond-axe"));
+
+            }
+          }
+        }
+      }
+    }
+  }
+}
