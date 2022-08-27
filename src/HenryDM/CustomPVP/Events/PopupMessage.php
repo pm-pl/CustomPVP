7f87fcd33a99057f5653354b71837a902a6e9de9
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-08-27 02:25:14 -0300

    Add PopupMessage

---
+++ src/HenryDM/CustomPVP/Events/PopupMessage.php
@@ -0,0 +1,87 @@
+<?php
+
+
+
+declare(strict_types=1);
+
+
+
+namespace HenryDM\CustomPVP\Events;
+
+
+
+use HenryDM\CustomPVP\Main;
+
+use function str_replace;
+
+
+
+use pocketmine\player\Player;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\entity\EntityDamageEvent;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+
+use pocketmine\event\player\PlayerDeathEvent;
+
+
+
+
+
+class PopupMessage implements Listener {
+
+
+
+    public function __construct(private Main $main) {
+
+        $this->main = $main;
+
+    }
+
+
+
+    public function onDeath(PlayerDeathEvent $event) : void {
+
+        if ($this->getMain()->cfg->get("PopupMessage") === true) {
+
+            $player = $event->getPlayer();
+
+            $cause = $player->getLastDamageCause();
+
+            if (in_array($worldName, $this->getMain()->cfg->getNested("killsound-worlds"))) {
+
+            if ($cause->getCause() == EntityDamageEvent::CAUSE_ENTITY_ATTACK) {
+
+                if ($cause instanceof EntityDamageByEntityEvent) {
+
+                    $damager = $cause->getDamager();
+
+                    if ($damager instanceof Player) {
+
+                        $message = str_replace(["{victim}", "{killer}"], [$event->getPlayer()->getName(), $damager->getName()], $this->getMain()->cfg->get("kill-popup-message"));
+
+                        $event->getPlayer()->sendPopup($message);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+    
+}
+
+
+
+    public function getMain() : Main {
+
+        return $this->main;
+
+    }
+
+}
