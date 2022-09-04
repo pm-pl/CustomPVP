e7a1838b5ea48937abf8758cb1c54d0f01bda63b
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-09-04 11:00:37 -0300

    bruh, BRuh, BRUh, BRUH

--- src/HenryDM/CustomPVP/Main.php
+++ src/HenryDM/CustomPVP/Main.php
@@ -47,6 +47,9 @@ class Main extends PluginBase implements Listener {
     public function onEnable() : void {
         $this->saveDefaultConfig();
         $this->cfg = $this->getConfig();
+        $this->saveResource("CustomEvents.yml");
+        $this->saveResource("KillEvents.yml");
+        $this->saveResource("ModerationEvents.yml");
         new Config($this->getDataFolder() . "CustomEvents.yml");
         new Config($this->getDataFolder() . "KillEvents.yml");
         new Config($this->getDataFolder() . "ModerationEvents.yml");
