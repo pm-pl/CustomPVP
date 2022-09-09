f583be1f3c1b8270065cd7b21829f13e863e242b
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-09-09 08:35:24 -0300

    Add KillEXP Event

---
+++ src/HenryDM/CustomPVP/Events/KillEXP.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HenryDM\CustomPVP\Events\KillEvents;
+
+use HenryDM\CustomPVP\Main;
+use pocketmine\event\Listener;
+
+use pocketmine\event\PlayerDeathEvent;
+
+class KillEXP implements Listener {
+
+    public function onDeath(PlayerDeathEvent $event) {
+# ====================================================
+       $player = $event->getPlayer();
+       $damageCause = $player->getLastDamageCause();
+       $damager = $damageCause->getDamager();
+       $worlds = $this->getMain()->cfg->get("soup-worlds", []);
+       $worldName = $event->getPlayer()->getWorld()->getDisplayName();
+       $xpvalue = $this->getMain()->cfg->get("xp-value");
+# ====================================================
+              if($this->getMain()->cfg->get("Kill-exp") === true) {
+                 if (in_array($worldName, $worlds, true)) {
+                     if($damageCause instanceof EntityDamageByEntityEvent) {
+                        if($damager instanceof Player) {
+                          $addXpLevel($damager, $xpvalue);
+        }
+     }
+  }
+}
+
